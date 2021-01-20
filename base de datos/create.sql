c16c2598b40a329c121d3f0051284bc22ad67593
estebanfabianp <38668876+estebanfabianp@users.noreply.github.com>
2021-01-19 20:25:14 -0500

    Se modifica la base de datos
    Se realiza una re estruturacion a la base de datos

--- base de datos/create.sql
+++ base de datos/create.sql
@@ -5,30 +5,13 @@
 # Project name:                                                          #
 # Author:                                                                #
 # Script type:           Database creation script                        #
-# Created on:            2021-01-14 23:34                                #
+# Created on:            2021-01-19 20:23                                #
 # ---------------------------------------------------------------------- #
 
 
 # ---------------------------------------------------------------------- #
 # Add tables                                                             #
 # ---------------------------------------------------------------------- #
-
-# ---------------------------------------------------------------------- #
-# Add table "estudiante"                                                 #
-# ---------------------------------------------------------------------- #
-
-CREATE TABLE `estudiante` (
-    `cedula` INTEGER(10) NOT NULL,
-    `nombre` VARCHAR(40),
-    `apellido` VARCHAR(40),
-    `telefono` VARCHAR(40),
-    `fechaNacimiento` DATE,
-    `Celular` INTEGER,
-    `tipoDocumento` VARCHAR(40),
-    `valor de matricula` INTEGER,
-    `descuento` INTEGER,
-    CONSTRAINT `PK_estudiante` PRIMARY KEY (`cedula`)
-);
 
 # ---------------------------------------------------------------------- #
 # Add table "nota"                                                       #
@@ -39,6 +22,18 @@ CREATE TABLE `nota` (
     `nota` VARCHAR(40),
     `valor` INTEGER,
     CONSTRAINT `PK_nota` PRIMARY KEY (`id_nota`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "materia"                                                    #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `materia` (
+    `id_materia` INTEGER NOT NULL AUTO_INCREMENT,
+    `notaFinal` INTEGER,
+    `habilitacion` INTEGER,
+    `nombreMateria` VARCHAR(40),
+    CONSTRAINT `PK_materia` PRIMARY KEY (`id_materia`)
 );
 
 # ---------------------------------------------------------------------- #
@@ -53,13 +48,82 @@ CREATE TABLE `Cargo` (
 );
 
 # ---------------------------------------------------------------------- #
-# Add table "ingreso"                                                    #
+# Add table "barrio"                                                     #
 # ---------------------------------------------------------------------- #
 
-CREATE TABLE `ingreso` (
-    `id_ingreso` INTEGER,
-    `cedula` INTEGER(10),
-    `tipoIngreso` VARCHAR(40)
+CREATE TABLE `barrio` (
+    `localidad` VARCHAR(40) NOT NULL,
+    `upz` VARCHAR(40) NOT NULL,
+    `barrio` VARCHAR(40) NOT NULL,
+    CONSTRAINT `PK_barrio` PRIMARY KEY (`localidad`, `upz`, `barrio`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "paquetes"                                                   #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `paquetes` (
+    `id_paquete` INTEGER NOT NULL,
+    `nombre` VARCHAR(40) NOT NULL,
+    `descriccion` TEXT,
+    `valor` VARCHAR(40),
+    `usuarioCreador` VARCHAR(40) NOT NULL,
+    `FechaCreador` DATE NOT NULL,
+    CONSTRAINT `PK_paquetes` PRIMARY KEY (`id_paquete`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "colegio"                                                    #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `colegio` (
+    `id_colegio` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(40) NOT NULL,
+    `telefono` VARCHAR(40),
+    CONSTRAINT `PK_colegio` PRIMARY KEY (`id_colegio`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "Check_list"                                                 #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `Check_list` (
+    `id_check` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre` VARCHAR(40) NOT NULL,
+    `validacion` CHAR(1) DEFAULT 'N',
+    CONSTRAINT `PK_Check_list` PRIMARY KEY (`id_check`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "estudiante"                                                 #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `estudiante` (
+    `id_estudiante` INTEGER NOT NULL AUTO_INCREMENT,
+    `tipoDocumento` VARCHAR(40) NOT NULL,
+    `numeroIdentifiacion` INTEGER(10) NOT NULL,
+    `nombre` VARCHAR(40) NOT NULL,
+    `apellido` VARCHAR(40) NOT NULL,
+    `telefono` VARCHAR(40) NOT NULL,
+    `Celular` INTEGER NOT NULL,
+    `fechaNacimiento` DATE NOT NULL,
+    `descuento` INTEGER,
+    `expedicion` VARCHAR(40),
+    `fotoCarnet` VARCHAR(40),
+    `Estrato` INTEGER,
+    `ocupacion` VARCHAR(40),
+    `otroTelefono` VARCHAR(40),
+    `correo` VARCHAR(40),
+    `ultimoCurso` INTEGER COMMENT 'ultimo curso aprobado',
+    `telefonoAcudiente` INTEGER,
+    `año` DATE COMMENT 'en que año curso el ultimo curo',
+    `observacion` TEXT,
+    `id_colegio` INTEGER,
+    `localidad` VARCHAR(40),
+    `upz` VARCHAR(40),
+    `barrio` VARCHAR(40),
+    `id_check` INTEGER NOT NULL,
+    CONSTRAINT `PK_estudiante` PRIMARY KEY (`id_estudiante`)
 );
 
 # ---------------------------------------------------------------------- #
@@ -78,35 +142,11 @@ CREATE TABLE `Personal` (
 );
 
 # ---------------------------------------------------------------------- #
-# Add table "Curso"                                                      #
-# ---------------------------------------------------------------------- #
-
-CREATE TABLE `Curso` (
-    `cedula` INTEGER(10) NOT NULL,
-    `horario` VARCHAR(40),
-    `identificacion` INTEGER NOT NULL
-);
-
-# ---------------------------------------------------------------------- #
-# Add table "materia"                                                    #
-# ---------------------------------------------------------------------- #
-
-CREATE TABLE `materia` (
-    `id_materia` INTEGER AUTO_INCREMENT,
-    `id_nota` INTEGER NOT NULL,
-    `notaFinal` INTEGER,
-    `habilitacion` INTEGER,
-    `nombreMateria` VARCHAR(40),
-    `identificacion` INTEGER NOT NULL,
-    CONSTRAINT `PK_materia` PRIMARY KEY (`identificacion`)
-);
-
-# ---------------------------------------------------------------------- #
 # Add table "egreso"                                                     #
 # ---------------------------------------------------------------------- #
 
 CREATE TABLE `egreso` (
-    `id_egreso` INTEGER AUTO_INCREMENT,
+    `id_egreso` INTEGER NOT NULL AUTO_INCREMENT,
     `nombre` VARCHAR(40),
     `valor` INTEGER,
     `fecha` DATETIME,
@@ -115,7 +155,44 @@ CREATE TABLE `egreso` (
     `perido` VARCHAR(40) COMMENT 'indica si es un  pago F , Recuernte',
     `dia` INTEGER,
     `frecuencia` VARCHAR(40) COMMENT 'indica si es diario , mesual o anual ',
-    `identificacion` INTEGER
+    `identificacion` INTEGER,
+    PRIMARY KEY (`id_egreso`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "matricula"                                                  #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `matricula` (
+    `id_matricula` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_estudiante` INTEGER,
+    `id_paquete` INTEGER,
+    `Descuento` INTEGER,
+    `observaciones` TEXT,
+    CONSTRAINT `PK_matricula` PRIMARY KEY (`id_matricula`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "pagos"                                                      #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `pagos` (
+    `id_pagos` INTEGER NOT NULL,
+    `id_matricula` INTEGER,
+    `valor` INTEGER,
+    `fecha` DATE,
+    `Concepto` VARCHAR(40),
+    CONSTRAINT `PK_pagos` PRIMARY KEY (`id_pagos`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "Curso"                                                      #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `Curso` (
+    `idCurso` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_matricula` INTEGER,
+    PRIMARY KEY (`idCurso`)
 );
 
 # ---------------------------------------------------------------------- #
@@ -123,27 +200,131 @@ CREATE TABLE `egreso` (
 # ---------------------------------------------------------------------- #
 
 CREATE TABLE `nomina` (
-    `id_nomina` INTEGER AUTO_INCREMENT
+    `id_nomina` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_egreso` INTEGER,
+    PRIMARY KEY (`id_nomina`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "grupos"                                                     #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `grupos` (
+    `id_grupos` INTEGER NOT NULL AUTO_INCREMENT,
+    `identificacion` INTEGER,
+    `idCurso` INTEGER,
+    CONSTRAINT `PK_grupos` PRIMARY KEY (`id_grupos`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "horarios"                                                   #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `horarios` (
+    `id_horario` INTEGER NOT NULL,
+    `Fecha` DATE,
+    `observacion` TEXT,
+    `id_grupos` INTEGER,
+    `identificacion` INTEGER,
+    CONSTRAINT `PK_horarios` PRIMARY KEY (`id_horario`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "clases"                                                     #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `clases` (
+    `id_clases` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_horario` INTEGER,
+    `id_nota` INTEGER,
+    `identificacion` INTEGER,
+    CONSTRAINT `PK_clases` PRIMARY KEY (`id_clases`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "asistencia"                                                 #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `asistencia` (
+    `id_asistencia` INTEGER NOT NULL,
+    `fecha` DATE,
+    `asistencia` CHAR(1) NOT NULL DEFAULT 'S',
+    `Incapacidad` TEXT,
+    `id_clases` INTEGER,
+    CONSTRAINT `PK_asistencia` PRIMARY KEY (`id_asistencia`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "matricula_asistencia"                                       #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `matricula_asistencia` (
+    `id_matricula` INTEGER NOT NULL,
+    `id_asistencia` INTEGER NOT NULL,
+    `Id_grupo_A` INTEGER NOT NULL AUTO_INCREMENT,
+    CONSTRAINT `PK_matricula_asistencia` PRIMARY KEY (`Id_grupo_A`)
 );
 
 # ---------------------------------------------------------------------- #
 # Add foreign key constraints                                            #
 # ---------------------------------------------------------------------- #
 
+ALTER TABLE `estudiante` ADD CONSTRAINT `colegio_estudiante` 
+    FOREIGN KEY (`id_colegio`) REFERENCES `colegio` (`id_colegio`);
+
+ALTER TABLE `estudiante` ADD CONSTRAINT `barrio_estudiante` 
+    FOREIGN KEY (`localidad`, `upz`, `barrio`) REFERENCES `barrio` (`localidad`,`upz`,`barrio`);
+
+ALTER TABLE `estudiante` ADD CONSTRAINT `Check_list_estudiante` 
+    FOREIGN KEY (`id_check`) REFERENCES `Check_list` (`id_check`);
+
 ALTER TABLE `Personal` ADD CONSTRAINT `Cargo_Personal` 
     FOREIGN KEY (`Id_cargo`) REFERENCES `Cargo` (`Id_cargo`);
 
-ALTER TABLE `Curso` ADD CONSTRAINT `estudiante_Curso` 
-    FOREIGN KEY (`cedula`) REFERENCES `estudiante` (`cedula`);
+ALTER TABLE `Curso` ADD CONSTRAINT `matricula_Curso` 
+    FOREIGN KEY (`id_matricula`) REFERENCES `matricula` (`id_matricula`);
 
-ALTER TABLE `Curso` ADD CONSTRAINT `Personal_Curso` 
-    FOREIGN KEY (`identificacion`) REFERENCES `Personal` (`identificacion`);
-
-ALTER TABLE `materia` ADD CONSTRAINT `nota_materia` 
-    FOREIGN KEY (`id_nota`) REFERENCES `nota` (`id_nota`);
-
-ALTER TABLE `ingreso` ADD CONSTRAINT `estudiante_ingreso` 
-    FOREIGN KEY (`cedula`) REFERENCES `estudiante` (`cedula`);
+ALTER TABLE `nomina` ADD CONSTRAINT `egreso_nomina` 
+    FOREIGN KEY (`id_egreso`) REFERENCES `egreso` (`id_egreso`);
 
 ALTER TABLE `egreso` ADD CONSTRAINT `Personal_egreso` 
     FOREIGN KEY (`identificacion`) REFERENCES `Personal` (`identificacion`);
+
+ALTER TABLE `matricula` ADD CONSTRAINT `estudiante_matricula` 
+    FOREIGN KEY (`id_estudiante`) REFERENCES `estudiante` (`id_estudiante`);
+
+ALTER TABLE `matricula` ADD CONSTRAINT `paquetes_matricula` 
+    FOREIGN KEY (`id_paquete`) REFERENCES `paquetes` (`id_paquete`);
+
+ALTER TABLE `grupos` ADD CONSTRAINT `Personal_grupos` 
+    FOREIGN KEY (`identificacion`) REFERENCES `Personal` (`identificacion`);
+
+ALTER TABLE `grupos` ADD CONSTRAINT `Curso_grupos` 
+    FOREIGN KEY (`idCurso`) REFERENCES `Curso` (`idCurso`);
+
+ALTER TABLE `horarios` ADD CONSTRAINT `grupos_horarios` 
+    FOREIGN KEY (`id_grupos`) REFERENCES `grupos` (`id_grupos`);
+
+ALTER TABLE `horarios` ADD CONSTRAINT `Personal_horarios` 
+    FOREIGN KEY (`identificacion`) REFERENCES `Personal` (`identificacion`);
+
+ALTER TABLE `clases` ADD CONSTRAINT `horarios_clases` 
+    FOREIGN KEY (`id_horario`) REFERENCES `horarios` (`id_horario`);
+
+ALTER TABLE `clases` ADD CONSTRAINT `nota_clases` 
+    FOREIGN KEY (`id_nota`) REFERENCES `nota` (`id_nota`);
+
+ALTER TABLE `clases` ADD CONSTRAINT `Personal_clases` 
+    FOREIGN KEY (`identificacion`) REFERENCES `Personal` (`identificacion`);
+
+ALTER TABLE `asistencia` ADD CONSTRAINT `clases_asistencia` 
+    FOREIGN KEY (`id_clases`) REFERENCES `clases` (`id_clases`);
+
+ALTER TABLE `matricula_asistencia` ADD CONSTRAINT `matricula_matricula_asistencia` 
+    FOREIGN KEY (`id_matricula`) REFERENCES `matricula` (`id_matricula`);
+
+ALTER TABLE `matricula_asistencia` ADD CONSTRAINT `asistencia_matricula_asistencia` 
+    FOREIGN KEY (`id_asistencia`) REFERENCES `asistencia` (`id_asistencia`);
+
+ALTER TABLE `pagos` ADD CONSTRAINT `matricula_pagos` 
+    FOREIGN KEY (`id_matricula`) REFERENCES `matricula` (`id_matricula`);
--- base de datos/drop.sql
+++ base de datos/drop.sql
@@ -5,7 +5,7 @@
 # Project name:                                                          #
 # Author:                                                                #
 # Script type:           Database drop script                            #
-# Created on:            2021-01-14 23:34                                #
+# Created on:            2021-01-19 20:23                                #
 # ---------------------------------------------------------------------- #
 
 
@@ -13,49 +13,175 @@
 # Drop foreign key constraints                                           #
 # ---------------------------------------------------------------------- #
 
+ALTER TABLE `estudiante` DROP FOREIGN KEY `colegio_estudiante`;
+
+ALTER TABLE `estudiante` DROP FOREIGN KEY `barrio_estudiante`;
+
+ALTER TABLE `estudiante` DROP FOREIGN KEY `Check_list_estudiante`;
+
 ALTER TABLE `Personal` DROP FOREIGN KEY `Cargo_Personal`;
 
-ALTER TABLE `Curso` DROP FOREIGN KEY `estudiante_Curso`;
+ALTER TABLE `Curso` DROP FOREIGN KEY `matricula_Curso`;
 
-ALTER TABLE `Curso` DROP FOREIGN KEY `Personal_Curso`;
-
-ALTER TABLE `materia` DROP FOREIGN KEY `nota_materia`;
-
-ALTER TABLE `ingreso` DROP FOREIGN KEY `estudiante_ingreso`;
+ALTER TABLE `nomina` DROP FOREIGN KEY `egreso_nomina`;
 
 ALTER TABLE `egreso` DROP FOREIGN KEY `Personal_egreso`;
+
+ALTER TABLE `matricula` DROP FOREIGN KEY `estudiante_matricula`;
+
+ALTER TABLE `matricula` DROP FOREIGN KEY `paquetes_matricula`;
+
+ALTER TABLE `grupos` DROP FOREIGN KEY `Personal_grupos`;
+
+ALTER TABLE `grupos` DROP FOREIGN KEY `Curso_grupos`;
+
+ALTER TABLE `horarios` DROP FOREIGN KEY `grupos_horarios`;
+
+ALTER TABLE `horarios` DROP FOREIGN KEY `Personal_horarios`;
+
+ALTER TABLE `clases` DROP FOREIGN KEY `horarios_clases`;
+
+ALTER TABLE `clases` DROP FOREIGN KEY `nota_clases`;
+
+ALTER TABLE `clases` DROP FOREIGN KEY `Personal_clases`;
+
+ALTER TABLE `asistencia` DROP FOREIGN KEY `clases_asistencia`;
+
+ALTER TABLE `matricula_asistencia` DROP FOREIGN KEY `matricula_matricula_asistencia`;
+
+ALTER TABLE `matricula_asistencia` DROP FOREIGN KEY `asistencia_matricula_asistencia`;
+
+ALTER TABLE `pagos` DROP FOREIGN KEY `matricula_pagos`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "matricula_asistencia"                                      #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `matricula_asistencia` MODIFY `Id_grupo_A` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `matricula_asistencia` DROP PRIMARY KEY;
+
+DROP TABLE `matricula_asistencia`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "asistencia"                                                #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `asistencia` ALTER COLUMN `asistencia` DROP DEFAULT;
+
+ALTER TABLE `asistencia` DROP PRIMARY KEY;
+
+DROP TABLE `asistencia`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "clases"                                                    #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `clases` MODIFY `id_clases` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `clases` DROP PRIMARY KEY;
+
+DROP TABLE `clases`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "horarios"                                                  #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `horarios` DROP PRIMARY KEY;
+
+DROP TABLE `horarios`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "grupos"                                                    #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `grupos` MODIFY `id_grupos` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `grupos` DROP PRIMARY KEY;
+
+DROP TABLE `grupos`;
 
 # ---------------------------------------------------------------------- #
 # Drop table "nomina"                                                    #
 # ---------------------------------------------------------------------- #
 
+# Remove autoinc for PK drop #
+
+ALTER TABLE `nomina` MODIFY `id_nomina` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `nomina` DROP PRIMARY KEY;
+
 DROP TABLE `nomina`;
-
-# ---------------------------------------------------------------------- #
-# Drop table "egreso"                                                    #
-# ---------------------------------------------------------------------- #
-
-# Drop constraints #
-
-DROP TABLE `egreso`;
-
-# ---------------------------------------------------------------------- #
-# Drop table "materia"                                                   #
-# ---------------------------------------------------------------------- #
-
-# Drop constraints #
-
-ALTER TABLE `materia` DROP PRIMARY KEY;
-
-DROP TABLE `materia`;
 
 # ---------------------------------------------------------------------- #
 # Drop table "Curso"                                                     #
 # ---------------------------------------------------------------------- #
 
+# Remove autoinc for PK drop #
+
+ALTER TABLE `Curso` MODIFY `idCurso` INTEGER NOT NULL;
+
 # Drop constraints #
 
+ALTER TABLE `Curso` DROP PRIMARY KEY;
+
 DROP TABLE `Curso`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "pagos"                                                     #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `pagos` DROP PRIMARY KEY;
+
+DROP TABLE `pagos`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "matricula"                                                 #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `matricula` MODIFY `id_matricula` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `matricula` DROP PRIMARY KEY;
+
+DROP TABLE `matricula`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "egreso"                                                    #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `egreso` MODIFY `id_egreso` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `egreso` DROP PRIMARY KEY;
+
+DROP TABLE `egreso`;
 
 # ---------------------------------------------------------------------- #
 # Drop table "Personal"                                                  #
@@ -68,12 +194,68 @@ ALTER TABLE `Personal` DROP PRIMARY KEY;
 DROP TABLE `Personal`;
 
 # ---------------------------------------------------------------------- #
-# Drop table "ingreso"                                                   #
+# Drop table "estudiante"                                                #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `estudiante` MODIFY `id_estudiante` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `estudiante` DROP PRIMARY KEY;
+
+DROP TABLE `estudiante`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "Check_list"                                                #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `Check_list` MODIFY `id_check` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `Check_list` ALTER COLUMN `validacion` DROP DEFAULT;
+
+ALTER TABLE `Check_list` DROP PRIMARY KEY;
+
+DROP TABLE `Check_list`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "colegio"                                                   #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `colegio` MODIFY `id_colegio` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `colegio` DROP PRIMARY KEY;
+
+DROP TABLE `colegio`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "paquetes"                                                  #
 # ---------------------------------------------------------------------- #
 
 # Drop constraints #
 
-DROP TABLE `ingreso`;
+ALTER TABLE `paquetes` DROP PRIMARY KEY;
+
+DROP TABLE `paquetes`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "barrio"                                                    #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `barrio` DROP PRIMARY KEY;
+
+DROP TABLE `barrio`;
 
 # ---------------------------------------------------------------------- #
 # Drop table "Cargo"                                                     #
@@ -90,6 +272,20 @@ ALTER TABLE `Cargo` DROP PRIMARY KEY;
 DROP TABLE `Cargo`;
 
 # ---------------------------------------------------------------------- #
+# Drop table "materia"                                                   #
+# ---------------------------------------------------------------------- #
+
+# Remove autoinc for PK drop #
+
+ALTER TABLE `materia` MODIFY `id_materia` INTEGER NOT NULL;
+
+# Drop constraints #
+
+ALTER TABLE `materia` DROP PRIMARY KEY;
+
+DROP TABLE `materia`;
+
+# ---------------------------------------------------------------------- #
 # Drop table "nota"                                                      #
 # ---------------------------------------------------------------------- #
 
@@ -102,13 +298,3 @@ ALTER TABLE `nota` MODIFY `id_nota` INTEGER NOT NULL;
 ALTER TABLE `nota` DROP PRIMARY KEY;
 
 DROP TABLE `nota`;
-
-# ---------------------------------------------------------------------- #
-# Drop table "estudiante"                                                #
-# ---------------------------------------------------------------------- #
-
-# Drop constraints #
-
-ALTER TABLE `estudiante` DROP PRIMARY KEY;
-
-DROP TABLE `estudiante`;
